32bbdcf74619ebd07074c0a701e3a70b192e701c
dev-hasin <hasinkiyani1@gmail.com>
2025-06-27 00:37:05 +0500

    expense-feature

---
+++ Expense.java
@@ -0,0 +1,33 @@
+package budgettracker;
+ import java.time.LocalDate;
+public class Expense {
+	private double amount;
+	private String category;
+	private LocalDate date;
+	
+	public Expense(double amount,String category,LocalDate date)
+	{
+		this.amount=amount;
+		this.category=category;
+		this.date=date;
+	}
+	public double getAmount()
+	{
+		return amount;
+	}
+	public String getCategory()
+	{
+		return category;
+	}
+	public LocalDate getDate()
+	{
+		return date;
+	}
+	
+	@Override
+	public String toString()
+	{
+		return date+" | "+category+" | "+amount;
+	}
+
+}
